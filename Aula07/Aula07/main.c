17bcb44d5a9be22a2f6a8a9c6a8021e554497fcf
Brunohugueninn <brunohugueninn@gmail.com>
2023-10-31 12:19:38 -0300

    aula 7

---
+++ Aula07/Aula07/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Aula 07
+    Lendo caracteres
+    função getchar();
+*/
+int main()
+{
+    char letra;
+
+    printf("Digite uma letra: ");
+    letra = getchar();
+
+    printf("Caracter lido: %c\n", letra);
+
+    return 0;
+}
