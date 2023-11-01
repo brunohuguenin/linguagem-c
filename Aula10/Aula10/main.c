95284965983822e6a73f547c44f450c3f0f6b682
Brunohugueninn <brunohugueninn@gmail.com>
2023-11-01 12:48:19 -0300

    aula 10

---
+++ Aula10/Aula10/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Aula 10
+    Lendo vários valores
+*/
+
+int main()
+{
+    int num1, num2, num3;
+
+    printf("Digite tres valores inteiros: ");
+    scanf("%d%d%d", &num1, &num2, &num3);
+
+    printf("Valores lidos: %d, %d e %d\n", num1, num2, num3);
+    return 0;
+}
