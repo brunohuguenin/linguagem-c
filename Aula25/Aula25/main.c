0995ed10c4cd67133f55f62a7a281f09ec37dab5
Brunohugueninn <brunohugueninn@gmail.com>
2023-11-03 16:15:58 -0300

    aula 25

---
+++ Aula25/Aula25/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Aula 25
+    Conversão de tipos
+*/
+
+int main()
+{
+    int a = 10, b = 20;
+
+    printf("\nSoma: %d\n", a + b);
+    printf("\nSubtraçao: %d\n", a - b);
+    printf("\nMultiplicaao: %d\n", a * b);
+
+    printf("\nDivisao: %f\n", (float)a / b);
+
+    return 0;
+}
