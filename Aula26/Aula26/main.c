7aa180147f595d565afdf6b4ef58ab83444a4a85
Brunohugueninn <brunohugueninn@gmail.com>
2023-11-03 16:16:12 -0300

    aula 26

---
+++ Aula26/Aula26/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Aula 26
+    Operador de resto da divisão
+*/
+
+int main()
+{
+    int a = 10, b = 20;
+
+    int y = 20, w = 7;
+
+    printf("\nSoma: %d\n", a + b);
+
+    printf("Resto da divisão inteira %d\n", 10 % 20);
+
+    printf("Resto da divisão inteira %d\n", b % a);
+    printf("Resto da divisão inteira %d\n", y % w);
+
+
+
+    return 0;
+}
