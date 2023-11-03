132a20246e11f9d72b71baa25921b8d4bea1ecaf
Brunohugueninn <brunohugueninn@gmail.com>
2023-11-03 16:15:17 -0300

    aula 24

---
+++ Aula24/Aula 24/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Aula 24
+    operadores matemáticos para:
+    Soma, Subtração, Multiplicação e Divisão;
+*/
+
+int main()
+{
+
+    printf("\nSoma: %d\n", 10 + 20);
+    printf("\nSubtraçao: %d\n", 894 - 300);
+    printf("\nMultiplicaao: %d\n", 25 * 25);
+    printf("\nDivisao: %d\n", 625 / 5);
+
+    printf("\nDivisao: %f\n", 10.0 / 20); // precisa-se transforma um número
+                                        // inteiro em real
+
+    return 0;
+}
