dfeb4edcdf7b40fc1154764c994bd382647cce0b
Brunohugueninn <brunohugueninn@gmail.com>
2023-11-12 20:19:17 -0300

    aula 64

---
+++ Aula64/Aula64/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Aula 64
+    Estrutura de repetição - for()
+*/
+
+int main()
+{
+    int i = 0;
+
+    for(i > 0; i <= 10; i++)
+        printf("%d\n\n", i);
+
+    return 0;
+}
