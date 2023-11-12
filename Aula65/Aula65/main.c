b64a4fb1a99389b930a165398355c239d4b2d1b9
Brunohugueninn <brunohugueninn@gmail.com>
2023-11-12 20:19:40 -0300

    aula 65

---
+++ Aula65/Aula65/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Aula 65
+    Estrutura de repetição -> for() decrescente
+*/
+
+int main()
+{
+    int i = 30;
+
+    for(i = 30; i > 0; i--)
+        printf("%d ", i);
+
+
+
+    return 0;
+}
