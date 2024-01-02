9e5868b62b1ca7ef523b815f36240a7d18589ecf
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-02 18:08:33 -0300

    aula 106

---
+++ Aula106/Aula106/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Aula 106
+    Como criar uma matriz?
+*/
+
+int main()
+{
+    // tipo indentificador
+    int vet[10];
+
+    int mat1[3][3] = {1,2,3,4,5,6,7,8,9};
+
+    int mat2[][3] = {1,2,3,4,5,6,7,};
+
+    int mat3[3[3] = {0};
+
+    return 0;
+}
