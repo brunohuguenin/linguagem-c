59da125d51baf17b56cffb6c112b88f9e3e50c0a
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-14 16:25:44 -0300

    aula 125

---
+++ Aula125/Aula125/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Aula 125
+    Dada uma matriz 4x4 faça um programa que diga se ele é
+    um Quadrado Mágico ou não
+
+    -Soma de todas as linhas, colunas diagonal principal e
+    secundária são iguais.
+*/
+
+int main()
+{
+    int matri
+    printf("Hello world!\n");
+    return 0;
+}
